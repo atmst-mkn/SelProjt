a7c72fa88e340274b9eb8a0041828a4b071cf953
atmkn_admin <manish.k.negi@gmail.com>
2020-01-09 21:47:16 +1300

    LoginTest2

---
+++ src/main/java/FeatureTwo/LoginTest2.java
@@ -0,0 +1,12 @@
+package FeatureTwo;
+
+public class LoginTest2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("LoginTest2");
+
+	}
+
+}
