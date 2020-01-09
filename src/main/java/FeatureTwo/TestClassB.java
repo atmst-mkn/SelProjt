c736d64de8378077d0f25d66920e65c3ce6dc63a
atmkn_admin <manish.k.negi@gmail.com>
2020-01-09 16:23:29 +1300

    first git commit project

---
+++ src/main/java/FeatureTwo/TestClassB.java
@@ -0,0 +1,9 @@
+package FeatureTwo;
+
+public class TestClassB {
+
+	public static void newTestB()
+	{
+		System.out.println("Test B");
+	}
+}
---
+++ src/main/java/atsel/ProjT/testclassA.java
@@ -0,0 +1,10 @@
+package atsel.ProjT;
+
+public class testclassA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("TestA");
+	}
+
+}
