43c6b43cec409f7db8785bb2cc736a5455e8340a
atmkn_admin <manish.k.negi@gmail.com>
2020-01-10 14:39:03 +1300

    new branch code

---
+++ src/test/java/atsel/ProjT/dealspageNew.java
@@ -0,0 +1,12 @@
+package atsel.ProjT;
+
+public class dealspageNew {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("DealsPageNew");
+
+	}
+
+}
